293a0d24ec08ad90000abb7c8b8bc72a5cc25c6c
Daniil Bagaturiya <121126156+Xapdina@users.noreply.github.com>
2023-09-15 15:40:49 +0300

    add brain_games/cli.py, fix brain_games/scripts/brain_games.py, poetry.lock and pyproject.toml

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,8 +1,13 @@
 #! /usr/bin/env python3
 
+from brain_games.cli import welcome_user
+
+
 def main():
-    print('Welome to the Brain Games')
+    print('Welcome to the Brain Games')
 
 
 if __name__ == '__main__':
     main()
+
+welcome_user()
