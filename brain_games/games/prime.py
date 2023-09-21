0ea13dca8f0b99d04050ed0b616febaa586a4d10
Daniil Bagaturiya <121126156+Xapdina@users.noreply.github.com>
2023-09-22 00:06:21 +0300

    hotfix brain_games/games/prime.py

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -6,17 +6,17 @@ TASK = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
 def prime(random_num):
+    if random_num <= 1:
+        return 'no'
     for i in range(2, (random_num // 2 + 1)):
-        if random_num % i == 0 or random_num <= 1:
-            correct_answer = 'no'
-            return correct_answer
-    else:
-        correct_answer = 'yes'
-        return correct_answer
+        if random_num % i == 0:
+            return 'no'
+        return 'yes'
+
 
 
 def question_and_answer():
-    random_num = randint(0, 20)
+    random_num = randint(0, 21)
     correct_answer = prime(random_num)
     question = f'{random_num}'
     return question, str(correct_answer)
